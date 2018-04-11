f8dbcd3d0bc135dcb8fa72693285ce5a85124578
Prem212 <38131181+Prem212@users.noreply.github.com>
2018-04-11 17:55:04 +0530

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,9 @@
+# String Functions
+# *******************
+prem = "what are"
+print(prem.replace("what","how"))
+print(":".join(["apple","mango","banana"]))
+print(prem.upper())
+print(prem.lower())
+print(prem.startswith("What"))
+print(prem.isnumeric())
